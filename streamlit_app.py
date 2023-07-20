6a80477b40aaec36f63df6bf08daab8e7ef28c41
sparklericha <140072253+sparklericha@users.noreply.github.com>
2023-07-20 19:02:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My first app!!!!! :) ')
 streamlit.header('🥗 🐔 🥑🍞Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')

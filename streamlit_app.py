ec62a4c41fef2d508f00c5d6cc29b20db67061d0
sparklericha <140072253+sparklericha@users.noreply.github.com>
2023-07-20 19:03:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥗 🐔Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.text('🥗 Bluberry and honey')
+streamlit.dataframe(my_fruit_list)
 

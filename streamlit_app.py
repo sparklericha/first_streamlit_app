d1938cdd0f69a0737f0840f8294fb39a38af3d40
sparklericha <140072253+sparklericha@users.noreply.github.com>
2023-07-20 19:10:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥗 🐔Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.dataframe(my_fruit_list)
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
+
 

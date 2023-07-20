62238a5d58dffc15fa48cb6dd99a92379a27ddce
sparklericha <140072253+sparklericha@users.noreply.github.com>
2023-07-20 18:58:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My first app!!!!! :) ')
 streamlit.header('🥗 🐔 🥑🍞Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
- streamlit.text(' 🥗 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🥗 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥗 🐔Hard-Boiled Free-Range Egg')
